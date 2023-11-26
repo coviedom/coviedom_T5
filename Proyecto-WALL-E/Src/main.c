7962f9eaea6fb8612cb2cc251f16c07d3afd7559
coviedom <coviedom@unal.edu.co>
2023-11-26 11:35:19 -0500

    los dos motores con el teclado

--- Proyecto-WALL-E/Src/main.c
+++ Proyecto-WALL-E/Src/main.c
@@ -21,10 +21,6 @@
 #include "arm_math.h"
 /*******************************************************************************************************************************************/
 
-/*Se hace los #define para calcular la frecuencia*/
-
-#define CANTIDAD_DE_SENSORES 2
-#define FRECUENCIA_DE_MUESTREO 40000
 /*Se encabeza la funcion a utilizar en el programa*/
 void start(void);
 /*Se define el Led rojo del blinky*/
@@ -38,41 +34,18 @@ USART_Handler_t usart = {0};
 uint8_t teclado = 0; /*variable que guarda el caracter recibido tras presionar una tecla*/
 /*Definimos el arreglo en el que se guardará el mensaje para cool Term*/
 char buffer_info[128] = {0};
-/*Se define los tres sensores tipo ADC_Config_t*/
-ADC_Config_t _Sensor1 = {0};  //PB1
-ADC_Config_t _Sensor2 = {0};  //PB0
-
-/*Se crea un arreglo que va a contener los sensores*/
-ADC_Config_t _Sensores[CANTIDAD_DE_SENSORES]= {0};
-/*Se crea el PWM que va ayudar a muestrear las señales*/
-
-
-
-//float32_t señal_A[TAMAÑO_DE_DATOS]; /*Arreglo de datos despues de muestrear la señal con el PWM*/
-//
-//float32_t señal_B[TAMAÑO_DE_DATOS]; /*Arreglo de datos despues de muestrear la señal con el PWM*/
-
-
-/*******************************************************************************************************************************************/
-/*Para la señal "A" Se define lo que nos ayudara a inciar y terminar la recoleccion de los datos*/
-uint16_t tecla_A = 0; /*Esta bandera inicia la recoleccion de los datos a la vez que se enciende el PWM*/
-uint16_t bandera_final_A = 0; /*Esta bandera indica que se ha termidado de recolectar los datos*/
-uint16_t contad_A = 0; /*Este contador es quien ayuda a recoger los datos hasta 512*/
-/*******************************************************************************************************************************************/
-/*Para la señal "B" Se define lo que nos ayudara a inciar y terminar la recoleccion de los datos*/
-uint16_t tecla_B = 0;/*Esta bandera inicia la recoleccion de los datos a la vez que se enciende el PWM*/
-uint16_t bandera_final_B = 0; /*Esta bandera indica que se ha termidado de recolectar los datos*/
-uint16_t contad_B = 0;  /*Este contador es quien ayuda a recoger los datos hasta 512*/
-/*******************************************************************************************************************************************/
-
-/*********************************************************************************/
-uint8_t _Contador_Secuencia = 0; /*Contador de la secuencia de conversion*/
 
 uint16_t _Duty = 500;
 PWM_Handler_t _PWM_ENA = {0};
 GPIO_Handler_t _ENA = {0};
 GPIO_Handler_t _IN1 = {0};
 GPIO_Handler_t _IN2 = {0};
+
+uint16_t _Duty2 = 500;
+PWM_Handler_t _PWM_ENB = {0};
+GPIO_Handler_t _ENB = {0};
+GPIO_Handler_t _IN3 = {0};
+GPIO_Handler_t _IN4 = {0};
 
 int main(void) {
 	/*Se activa el co-procesador FPU*/
@@ -83,66 +56,99 @@ int main(void) {
 	while (1) {
 /*******************************************************************************************************************************************/
 
-/*******************************************************************************************************************************************/
-        /*Lo siguiente ocurre si presiona una letra del teclado*/
+		/*Lo siguiente ocurre si presiona una letra del teclado*/
 		if (teclado != '\0') {
 			if (teclado == 'x'){
 				_Duty -= 50;
-				if (_Duty < 50){
+				_Duty2 -= 50;
+				if (_Duty < 50 || _Duty2 < 50){
 					_Duty = 50;
+					_Duty2 = 50;
+
 				}
 				sprintf(buffer_info, "Dutty =%u \n",(unsigned int)_Duty);
 				usart_writeMsg(&usart, buffer_info);
 				teclado = 0;
 
 			}
-			if (teclado == 'a'){
+			if (teclado == 'z'){
 				_Duty += 50;
-				if (_Duty > 1000){
+				_Duty2 += 50;
+				if (_Duty > 1000 || _Duty2 > 1000){
 					_Duty = 1000;
+					_Duty2 = 1000;
 				}
 				sprintf(buffer_info, "Dutty =%u \n",(unsigned int)_Duty);
 				usart_writeMsg(&usart, buffer_info);
 				teclado = 0;
 
 			}
-
+			actualiza_Ciclo_Duty(&_PWM_ENB, _Duty2);
 			actualiza_Ciclo_Duty(&_PWM_ENA, _Duty);
 
+
 			/*Si se presiona la letra "a"*/
-			if (teclado == 'b') {
-				usart_writeMsg(&usart,"prende IN1\n\r");
-
-				gpio_WritePin(&_IN1,SET);
-
-				teclado = 0;
-			}
-			/*Si se presiona la letra "b"*/
-			if (teclado == 'c') {
-				usart_writeMsg(&usart,"apaga IN1\n\r");
+			if (teclado == 'w') {
+				usart_writeMsg(&usart,"Adelante\n\r");
 				gpio_WritePin(&_IN1,RESET);
-
-				/*Se enciende el PWM de muestreo*/
-//				inicio_de_señal_pwm(&_PWM_Muestreo);
-				/*Se sube la bandera que inicia la recoleccion de datos para la señal del sensor 2*/
-//				tecla_B = SET;
-				/*Se hace cero a teclado para poder volver a ingresar la letra si asi lo quiere*/
-				teclado = 0;
-			}
-
-			if (teclado == 'd') {
-				usart_writeMsg(&usart,"prende IN2\n\r");
 				gpio_WritePin(&_IN2,SET);
+				gpio_WritePin(&_IN3,RESET);
+				gpio_WritePin(&_IN4,SET);
 				teclado = 0;
 			}
 			/*Si se presiona la letra "b"*/
-			if (teclado == 'e') {
-				usart_writeMsg(&usart,"apaga IN2\n\r");
+			else if (teclado == 's') {
+				usart_writeMsg(&usart,"Atrás\n\r");
+				gpio_WritePin(&_IN1,SET);
 				gpio_WritePin(&_IN2,RESET);
+				gpio_WritePin(&_IN3,SET);
+				gpio_WritePin(&_IN4,RESET);
 				teclado = 0;
 			}
-			if (teclado == 'r') {
-				usart_writeMsg(&usart,"funciona\n\r");
+
+			else if (teclado == 'a') {
+				usart_writeMsg(&usart,"Izquierda\n\r");
+				gpio_WritePin(&_IN1,RESET);
+				gpio_WritePin(&_IN2,RESET);
+				gpio_WritePin(&_IN3,RESET);
+				gpio_WritePin(&_IN4,SET);
+				teclado = 0;
+			}
+			/*Si se presiona la letra "b"*/
+			else if (teclado == 'd') {
+				usart_writeMsg(&usart,"Derecha\n\r");
+				gpio_WritePin(&_IN1,RESET);
+				gpio_WritePin(&_IN2,SET);
+				gpio_WritePin(&_IN3,RESET);
+				gpio_WritePin(&_IN4,RESET);
+				teclado = 0;
+			}
+			else if (teclado == 'g') {
+				usart_writeMsg(&usart,"360 a la Izquierda\n\r");
+				gpio_WritePin(&_IN1,SET);
+				gpio_WritePin(&_IN2,RESET);
+				gpio_WritePin(&_IN3,RESET);
+				gpio_WritePin(&_IN4,SET);
+				teclado = 0;
+			}
+			else if (teclado == 'h') {
+				usart_writeMsg(&usart,"360 a la derecha\n\r");
+				gpio_WritePin(&_IN1,RESET);
+				gpio_WritePin(&_IN2,SET);
+				gpio_WritePin(&_IN3,SET);
+				gpio_WritePin(&_IN4,RESET);
+				teclado = 0;
+			}
+			else if (teclado == 'o') {
+				usart_writeMsg(&usart,"APAGA\n\r");
+				gpio_WritePin(&_IN1,RESET);
+				gpio_WritePin(&_IN2,RESET);
+				gpio_WritePin(&_IN3,RESET);
+				gpio_WritePin(&_IN4,RESET);
+				teclado = 0;
+			}
+			else if (teclado == 'r') {
+				usart_writeMsg(&usart,"Funciona\n\r");
 				teclado = 0;
 			}
 			/*Si se presiona la letra "c"**/
@@ -195,50 +201,54 @@ void start(void) {
 	Receptor.pinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
 	Receptor.pinConfig.GPIO_PinAltFunMode = AF7;
 	gpio_Config(&Receptor);
-	/*Enseguida se van a configurar los 3 canales ADC*/
-	/*El sensor 1 siendo el primero de la secuencia*/
-//	_Sensor1.channel = CHANNEL_9;
-//	_Sensor1.resolution = RESOLUTION_12_BIT;
-//	_Sensor1.dataAlignment = ALIGNMENT_RIGHT;
-//	_Sensor1.samplingPeriod = SAMPLING_PERIOD_112_CYCLES;
-//	_Sensor1.interrupState = ADC_INT_ENABLE;
-//	/*El sensor 2 siendo el segundo de la secuencia*/
-//	_Sensor2.channel = CHANNEL_8;
-//	_Sensor2.resolution = RESOLUTION_12_BIT;
-//	_Sensor2.dataAlignment = ALIGNMENT_RIGHT;
-//	_Sensor2.samplingPeriod = SAMPLING_PERIOD_112_CYCLES;
-//	_Sensor2.interrupState = ADC_INT_ENABLE;
-//	/*El sensor 3 siendo el tercero de la secuencia*/
-//
-//	/*se asigna la posicion en el arreglo segun la secuencia */
-//	_Sensores[0] = _Sensor1;
-//	_Sensores[1] = _Sensor2;
 
-	/*Ahora que se cargue la configuracion de todos los sensores*/
-//	adc_ConfigMultichannel (_Sensores,CANTIDAD_DE_SENSORES);
-	/*Se configura el PWM que va a ayudar a muestrear la señal */
+	_ENB.pGPIOx = GPIOC;
+	_ENB.pinConfig.GPIO_PinNumber = PIN_7;
+    _ENB.pinConfig.GPIO_PinMode = GPIO_MODE_ALTFN;
+	_ENB.pinConfig.GPIO_PinOutputType = GPIO_OTYPE_PUSHPULL;
+	_ENB.pinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
+	_ENB.pinConfig.GPIO_PinAltFunMode = AF2;
+	gpio_Config(&_ENB);
+
+	_PWM_ENB.pTIMx = TIM3;
+	_PWM_ENB.config.Canal = PWM_CHANNEL_2;
+	_PWM_ENB.config.prescaler = 16;
+	_PWM_ENB.config.periodo = 1000;
+	_PWM_ENB.config.CicloDuty = 500;
+	configuracion_del_pwm(&_PWM_ENB);
+	activar_salida(&_PWM_ENB);
+	inicio_de_señal_pwm(&_PWM_ENB);
+
+	_IN4.pGPIOx = GPIOA;
+	_IN4.pinConfig.GPIO_PinNumber = PIN_12;
+	_IN4.pinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	_IN4.pinConfig.GPIO_PinOutputType = GPIO_OTYPE_PUSHPULL;
+	_IN4.pinConfig.GPIO_PinOutputSpeed = GPIO_OSPEED_FAST;
+	_IN4.pinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
+	gpio_Config(&_IN4);
+	gpio_WritePin(&_IN4,RESET);
+
+	_IN3.pGPIOx = GPIOC;
+	_IN3.pinConfig.GPIO_PinNumber = PIN_5;
+	_IN3.pinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	_IN3.pinConfig.GPIO_PinOutputType = GPIO_OTYPE_PUSHPULL;
+	_IN3.pinConfig.GPIO_PinOutputSpeed = GPIO_OSPEED_FAST;
+	_IN3.pinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
+	gpio_Config(&_IN3);
+	gpio_WritePin(&_IN3,RESET);
 
 
+	_IN2.pGPIOx = GPIOC;
+	_IN2.pinConfig.GPIO_PinNumber = PIN_9;
+	_IN2.pinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	_IN2.pinConfig.GPIO_PinOutputType = GPIO_OTYPE_PUSHPULL;
+	_IN2.pinConfig.GPIO_PinOutputSpeed = GPIO_OSPEED_FAST;
+	_IN2.pinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
+	gpio_Config(&_IN2);
+	gpio_WritePin(&_IN2,RESET);
 
-	_ENA.pGPIOx = GPIOC;
-	_ENA.pinConfig.GPIO_PinNumber = PIN_7;
-    _ENA.pinConfig.GPIO_PinMode = GPIO_MODE_ALTFN;
-	_ENA.pinConfig.GPIO_PinOutputType = GPIO_OTYPE_PUSHPULL;
-	_ENA.pinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
-	_ENA.pinConfig.GPIO_PinAltFunMode = AF2;
-	gpio_Config(&_ENA);
-
-	_PWM_ENA.pTIMx = TIM3;
-	_PWM_ENA.config.Canal = PWM_CHANNEL_2;
-	_PWM_ENA.config.prescaler = 16;
-	_PWM_ENA.config.periodo = 1000;
-	_PWM_ENA.config.CicloDuty = 500;
-	configuracion_del_pwm(&_PWM_ENA);
-	activar_salida(&_PWM_ENA);
-	inicio_de_señal_pwm(&_PWM_ENA);
-
-	_IN1.pGPIOx = GPIOA;
-	_IN1.pinConfig.GPIO_PinNumber = PIN_12;
+	_IN1.pGPIOx = GPIOB;
+	_IN1.pinConfig.GPIO_PinNumber = PIN_6;
 	_IN1.pinConfig.GPIO_PinMode = GPIO_MODE_OUT;
 	_IN1.pinConfig.GPIO_PinOutputType = GPIO_OTYPE_PUSHPULL;
 	_IN1.pinConfig.GPIO_PinOutputSpeed = GPIO_OSPEED_FAST;
@@ -246,14 +256,22 @@ void start(void) {
 	gpio_Config(&_IN1);
 	gpio_WritePin(&_IN1,RESET);
 
-	_IN2.pGPIOx = GPIOC;
-	_IN2.pinConfig.GPIO_PinNumber = PIN_5;
-	_IN2.pinConfig.GPIO_PinMode = GPIO_MODE_OUT;
-	_IN2.pinConfig.GPIO_PinOutputType = GPIO_OTYPE_PUSHPULL;
-	_IN2.pinConfig.GPIO_PinOutputSpeed = GPIO_OSPEED_FAST;
-	_IN2.pinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
-	gpio_Config(&_IN2);
-	gpio_WritePin(&_IN2,RESET);
+	_ENA.pGPIOx = GPIOA;
+	_ENA.pinConfig.GPIO_PinNumber = PIN_5;
+    _ENA.pinConfig.GPIO_PinMode = GPIO_MODE_ALTFN;
+	_ENA.pinConfig.GPIO_PinOutputType = GPIO_OTYPE_PUSHPULL;
+	_ENA.pinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
+	_ENA.pinConfig.GPIO_PinAltFunMode = AF1;
+	gpio_Config(&_ENA);
+
+	_PWM_ENA.pTIMx = TIM2;
+	_PWM_ENA.config.Canal = PWM_CHANNEL_1;
+	_PWM_ENA.config.prescaler = 16;
+	_PWM_ENA.config.periodo = 1000;
+	_PWM_ENA.config.CicloDuty = 500;
+	configuracion_del_pwm(&_PWM_ENA);
+	activar_salida(&_PWM_ENA);
+	inicio_de_señal_pwm(&_PWM_ENA);
 
 
 }
@@ -269,76 +287,6 @@ void usart2_RxCallback(void) {
 }
 /*Esta funcion se activa por cada conversion segun la secuencia*/
 void adc_CompleteCallback(void) {
-//	/*Dependiendo de la secuencia de conversion, es decir del Contador Secuencia, se hace la toma del dato*/
-//	_Sensores[_Contador_Secuencia].adcData = adc_GetValue();
-//
-///*******************************************************************************************************************************************/
-//	/*Si se ha presionado la tecla "a" ocurrira lo siguiente */
-//	if (tecla_A) {
-//		/*Se evalua si se acaba de hacer una sola conversion del sensor 1*/
-//		if (_Contador_Secuencia == 0) {
-//			/*Se almacena secuencialmente el dato ADC del sensor 1 en el arreglo de 512 datos llamado Señal A*/
-//			señal_A[contad_A] = _Sensores[0].adcData;
-//			/*Se incrementa este contador con el fin de llenar el arreglo hasta 512*/
-//			contad_A++;
-//			/*Se evalua si se llega a 512 datos*/
-//			if (contad_A == TAMAÑO_DE_DATOS) {
-//				/*Se reinicia el contador en caso de que quiera por accion de la tecla "a" volver a recolectar datos*/
-//				contad_A = 0;
-//				/*Se levanta la bandera para que no se siga recolectando los datos en este arreglo Señal A*/
-//				tecla_A = RESET;
-//				/*Se levanta la bandera indicando que se terminó la recolección*/
-//				bandera_final_A = SET;
-//			}
-//		}
-//	}
-///*******************************************************************************************************************************************/
-//	/*Si se ha presionado la tecla "b" ocurrira lo siguiente */
-//	if (tecla_B) {
-//		/*Se evalua si se acaba de hacer una sola conversion del sensor 2*/
-//		if (_Contador_Secuencia == 1) {
-//			/*Se almacena secuencialmente el dato ADC del sensor 2 en el arreglo de 512 datos llamado Señal B*/
-//			señal_B[contad_B] = _Sensores[1].adcData;
-//			/*Se incrementa este contador con el fin de llenar el arreglo hasta 512*/
-//			contad_B++;
-//			/*Se evalua si se llega a 512 datos*/
-//			if (contad_B == TAMAÑO_DE_DATOS) {
-//				/*Se reinicia el contador en caso de que quiera por accion de la tecla "b" volver a recolectar datos*/
-//				contad_B = 0;
-//				/*Se levanta la bandera para que no se siga recolectando los datos en este arreglo Señal B*/
-//				tecla_B = RESET;
-//				/*Se levanta la bandera indicando que se terminó la recolección*/
-//				bandera_final_B = SET;
-//			}
-//		}
-//	}
-///*******************************************************************************************************************************************/
-//	/*Si se ha presionado la tecla "c" ocurrira lo siguiente */
-//	if (tecla_C) {
-//		/*Se evalua si se acaba de hacer una sola conversion del sensor 3*/
-//		if (_Contador_Secuencia == 2) {
-//			/*Se almacena secuencialmente el dato ADC del sensor 3 en el arreglo de 512 datos llamado Señal C*/
-//			señal_C[contad_C] = _Sensores[2].adcData;
-//			/*Se incrementa este contador con el fin de llenar el arreglo hasta 512*/
-//			contad_C++;
-//			/*Se evalua si se llega a 512 datos*/
-//			if (contad_C == TAMAÑO_DE_DATOS) {
-//				/*Se reinicia el contador en caso de que quiera por accion de la tecla "c" volver a recolectar datos*/
-//				contad_C = 0;
-//				/*Se levanta la bandera para que no se siga recolectando los datos en este arreglo Señal C*/
-//				tecla_C = RESET;
-//				/*Se levanta la bandera indicando que se terminó la recolección*/
-//				bandera_final_C = SET;
-//			}
-//		}
-//	}
-///*******************************************************************************************************************************************/
-//	/*Se incrementa el contador para llevar la cuenta de la secuencia de conversion ADC*/
-//	_Contador_Secuencia++;
-//	/*Se evalua si la secuencia ha llegado a 3, el cual es el numero de canales*/
-//	if (_Contador_Secuencia >=CANTIDAD_DE_SENSORES){
-//		/*Se reinicia el contador para estar al ritmo de la secuencia de conversion ADC*/
-//		_Contador_Secuencia = 0;
-//	}
+	__NOP();
 }
 /*FINISH*//*********************************************************************************************************************************/
